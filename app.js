16736f6ab74078192f3d739c4bd8b8fc59e84579
vh-fullstack <109882814+vh-fullstack@users.noreply.github.com>
2025-11-10 22:20:07 -0500

    modify fly.toml for http  check

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.get('/version', (req, res) => {
   res.send('1') // change this string to ensure a new version deployed
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
-  this_causes_error
   console.log(`server started on port ${PORT}`)
 })
