846ed6fa7e94445aaea880852bbc53e2497172fd
vh-fullstack <109882814+vh-fullstack@users.noreply.github.com>
2025-11-10 20:05:42 -0500

    test for visiting other pages

--- e2e-tests/app.spec.js
+++ e2e-tests/app.spec.js
@@ -6,12 +6,8 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('http://localhost:8080/pokemon/ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
-
-test('front page can be opened', async ({ page }) => {
-  await page.goto('http://localhost:8080')
-
-  const locator = page.getByText('bulbasaur')
-  await expect(locator).toBeVisible()
-})
-
