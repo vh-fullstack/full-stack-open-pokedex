9a3fd529be991154ab65ac5bd7ef6822c5ac653b
vh-fullstack <109882814+vh-fullstack@users.noreply.github.com>
2025-12-11 21:16:38 -0500

    Modify Playwright config to auto-start a local dev server

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
 
   testIgnore: [
-    'example.spec.js'
+    'examples/**'
   ],
   testDir: './e2e-tests',
   /* Run tests in files in parallel */
@@ -31,7 +31,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -76,10 +76,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:8080',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
