be872bbf97bf64f79766ea33f34302b07ea01bbd
vh-fullstack <109882814+vh-fullstack@users.noreply.github.com>
2025-12-11 21:25:04 -0500

    Remove example test folder from linting process

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -82,5 +82,5 @@ export default defineConfig({
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
-});
+})
 
