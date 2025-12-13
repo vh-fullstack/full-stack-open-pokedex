9cce883e755f6294eab3af07b5c6271b95dd90e1
vh-fullstack <109882814+vh-fullstack@users.noreply.github.com>
2025-12-13 15:25:49 -0500

    Add hello world route

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/hello', (req, res) => {
+  res.send('hello, world!')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
