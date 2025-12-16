53c85fbb2efb5543908fba2e073361f0fd9df861
vh-fullstack <109882814+vh-fullstack@users.noreply.github.com>
2025-12-16 13:37:21 -0500

    fix: remove the branch in the spokes

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.get('/health', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  branch in the wheels
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
